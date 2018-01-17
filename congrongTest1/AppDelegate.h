154882bc760b00eb3917528ba660c5c9123b9904
congrong <1349375254@qq.com>
2018-01-17 11:41:43 +0800

    初始化

---
+++ congrongTest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  congrongTest1
+//
+//  Created by 王从容 on 2018/1/17.
+//  Copyright © 2018年 王从容. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ congrongTest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  congrongTest1
+//
+//  Created by 王从容 on 2018/1/17.
+//  Copyright © 2018年 王从容. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
